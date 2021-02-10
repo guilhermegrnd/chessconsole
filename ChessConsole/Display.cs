0224227edbbe43f146d16f10bb92009119a583ec
guilhermegrnd <guilhermegranado@outlook.com>
2021-02-10 20:26:14 -0300

    Added print Board Function

--- ChessConsole/Display.cs
+++ ChessConsole/Display.cs
@@ -5,14 +5,28 @@ namespace ChessConsole {
     class Display {
         public static void printBoard(Board board) {
             for (int i = 0; i < board.rows; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.columns; j++) {
                     if(board.piece(i,j) == null) {
                         Console.Write("- ");
                     } else {
-                        Console.Write(board.piece(i, j) + " ");
+                        printPiece(board.piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine("");
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void printPiece(Piece piece) {
+            if(piece.color == Color.White) {
+                Console.Write(piece);
+            } else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -6,11 +6,14 @@ namespace ChessConsole {
     class Program {
         static void Main(string[] args) {
             try {
-                chessPosition pos = new chessPosition('H', 3);
+                Board b = new Board(8, 8);
 
-                Console.WriteLine(pos);
+                b.placePiece(new Rook(b, Color.White), new Position(0, 0));
+                b.placePiece(new King(b, Color.White), new Position(1, 3));
+                b.placePiece(new Rook(b, Color.Black), new Position(4, 6));
+                b.placePiece(new King(b, Color.Black), new Position(7, 4));
 
-                Console.WriteLine(pos.toPosition());
+                Display.printBoard(b);
 
                 Console.ReadLine();
             } catch(BoardException e) {
