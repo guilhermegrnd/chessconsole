6985eb9799eab0db5266bd24398157560194a520
guilhermegrnd <guilhermegranado@outlook.com>
2021-02-17 23:15:11 -0300

    En Passant Move Implemented

--- ChessConsole/chess/ChessMatch.cs
+++ ChessConsole/chess/ChessMatch.cs
@@ -5,20 +5,22 @@ namespace chess {
     class ChessMatch {
         public Board board { get; private set; }
         public int move { get; private set; }
-        public  Color currentPlayer { get; private set; }
+        public Color currentPlayer { get; private set; }
         public bool gameOver { get; private set; }
         private HashSet<Piece> pieces;
         private HashSet<Piece> taken;
         public bool check { get; private set; }
+        public Piece vulnerableEnPassant { get; private set; }
 
         public ChessMatch() {
             board = new Board(8, 8);
             move = 1;
             currentPlayer = Color.White;
             gameOver = false;
+            check = false;
+            vulnerableEnPassant = null;
             pieces = new HashSet<Piece>();
             taken = new HashSet<Piece>();
-            check = false;
             placePieces();
         }
 
@@ -27,7 +29,7 @@ namespace chess {
             p.incrementMovesMade();
             Piece pieceTaken = board.takePiece(destination);
             board.placePiece(p, destination);
-            if(pieceTaken != null) {
+            if (pieceTaken != null) {
                 taken.Add(pieceTaken);
             }
 
@@ -48,13 +50,28 @@ namespace chess {
                 board.placePiece(rook, destinationRook);
             }
 
+            //EN PASSANT
+            if (p is Pawn) {
+                if (origin.column != destination.column && pieceTaken == null) {
+                    Position posPawn;
+                    if (p.color == Color.White) {
+                        posPawn = new Position(destination.row + 1, destination.column);
+                    }
+                    else {
+                        posPawn = new Position(destination.row - 1, destination.column);
+                    }
+                    pieceTaken = board.takePiece(posPawn);
+                    taken.Add(pieceTaken);
+                }
+            }
+
             return pieceTaken;
         }
 
         public void undoMove(Position origin, Position destination, Piece pieceTaken) {
             Piece p = board.takePiece(destination);
             p.decrementMovesMade();
-            if(pieceTaken != null) {
+            if (pieceTaken != null) {
                 board.placePiece(pieceTaken, destination);
                 taken.Remove(pieceTaken);
             }
@@ -76,13 +93,28 @@ namespace chess {
                 rook.decrementMovesMade();
                 board.placePiece(rook, originRook);
             }
+
+            //EN PASSANT
+            if (p is Pawn) {
+                if (origin.column != destination.column && pieceTaken == vulnerableEnPassant) {
+                    Piece pawn = board.takePiece(destination);
+                    Position posPawn;
+                    if (p.color == Color.White) {
+                        posPawn = new Position(3, destination.column);
+                    }
+                    else {
+                        posPawn = new Position(4, destination.column);
+                    }
+                    board.placePiece(pawn, posPawn);
+                }
+            }
         }
 
         public void executePlay(Position origin, Position destination) {
             Piece pieceTaken = executeMove(origin, destination);
 
-            if(inCheck(currentPlayer)) {
-                undoMove(origin,destination,pieceTaken);
+            if (inCheck(currentPlayer)) {
+                undoMove(origin, destination, pieceTaken);
                 throw new BoardException("Você não pode se colocar em xeque!");
             }
 
@@ -92,11 +124,19 @@ namespace chess {
                 check = false;
             }
 
-            if(checkMate(adversary(currentPlayer))) {
+            if (checkMate(adversary(currentPlayer))) {
                 gameOver = true;
             } else {
                 move++;
                 changeCurrentPlayer();
+            }
+
+            //EN PASSANT
+            Piece p = board.piece(destination);
+            if (p is Pawn && (destination.row == origin.row - 2 && destination.row == origin.row + 2)) {
+                vulnerableEnPassant = p;
+            } else {
+                vulnerableEnPassant = null;
             }
         }
 
@@ -218,14 +258,14 @@ namespace chess {
             placePiece('F', 1, new Bishop(board, Color.White));
             placePiece('G', 1, new Knight(board, Color.White));
             placePiece('H', 1, new Rook(board, Color.White));
-            placePiece('A', 2, new Pawn(board, Color.White));
-            placePiece('B', 2, new Pawn(board, Color.White));
-            placePiece('C', 2, new Pawn(board, Color.White));
-            placePiece('D', 2, new Pawn(board, Color.White));
-            placePiece('E', 2, new Pawn(board, Color.White));
-            placePiece('F', 2, new Pawn(board, Color.White));
-            placePiece('G', 2, new Pawn(board, Color.White));
-            placePiece('H', 2, new Pawn(board, Color.White));
+            placePiece('A', 2, new Pawn(board, Color.White, this));
+            placePiece('B', 2, new Pawn(board, Color.White, this));
+            placePiece('C', 2, new Pawn(board, Color.White, this));
+            placePiece('D', 2, new Pawn(board, Color.White, this));
+            placePiece('E', 2, new Pawn(board, Color.White, this));
+            placePiece('F', 2, new Pawn(board, Color.White, this));
+            placePiece('G', 2, new Pawn(board, Color.White, this));
+            placePiece('H', 2, new Pawn(board, Color.White, this));
 
             placePiece('A', 8, new Rook(board, Color.Black));
             placePiece('B', 8, new Knight(board, Color.Black));
@@ -235,14 +275,14 @@ namespace chess {
             placePiece('F', 8, new Bishop(board, Color.Black));
             placePiece('G', 8, new Knight(board, Color.Black));
             placePiece('H', 8, new Rook(board, Color.Black));
-            placePiece('A', 7, new Pawn(board, Color.Black));
-            placePiece('B', 7, new Pawn(board, Color.Black));
-            placePiece('C', 7, new Pawn(board, Color.Black));
-            placePiece('D', 7, new Pawn(board, Color.Black));
-            placePiece('E', 7, new Pawn(board, Color.Black));
-            placePiece('F', 7, new Pawn(board, Color.Black));
-            placePiece('G', 7, new Pawn(board, Color.Black));
-            placePiece('H', 7, new Pawn(board, Color.Black));
+            placePiece('A', 7, new Pawn(board, Color.Black, this));
+            placePiece('B', 7, new Pawn(board, Color.Black, this));
+            placePiece('C', 7, new Pawn(board, Color.Black, this));
+            placePiece('D', 7, new Pawn(board, Color.Black, this));
+            placePiece('E', 7, new Pawn(board, Color.Black, this));
+            placePiece('F', 7, new Pawn(board, Color.Black, this));
+            placePiece('G', 7, new Pawn(board, Color.Black, this));
+            placePiece('H', 7, new Pawn(board, Color.Black, this));
         }
     }
 }
--- ChessConsole/chess/Pawn.cs
+++ ChessConsole/chess/Pawn.cs
@@ -2,7 +2,10 @@
 
 namespace chess {
     class Pawn : Piece {
-        public Pawn(Board board, Color color) : base(board, color) {
+
+        private ChessMatch match;
+        public Pawn(Board board, Color color, ChessMatch match) : base(board, color) {
+            this.match = match;
         }
 
         public override string ToString() {
@@ -40,6 +43,18 @@ namespace chess {
                 if (board.validPosition(pos) && enemyAhead(pos)) {
                     boolMatrix[pos.row, pos.column] = true;
                 }
+
+                //EN PASSANT
+                if(position.row == 3) {
+                    Position left = new Position(position.row, position.column - 1);
+                    if (board.validPosition(left) && enemyAhead(left) && board.piece(left) == match.vulnerableEnPassant) {
+                        boolMatrix[left.row - 1, left.column] = true;
+                    }
+                    Position right = new Position(position.row, position.column + 1);
+                    if (board.validPosition(right) && enemyAhead(right) && board.piece(right) == match.vulnerableEnPassant) {
+                        boolMatrix[right.row - 1, right.column] = true;
+                    }
+                }
             } else {
                 pos.changePosition(position.row + 1, position.column);
                 if (board.validPosition(pos) && clearSquare(pos)) {
@@ -56,6 +71,17 @@ namespace chess {
                 pos.changePosition(position.row + 1, position.column + 1);
                 if (board.validPosition(pos) && enemyAhead(pos)) {
                     boolMatrix[pos.row, pos.column] = true;
+                }
+                //EN PASSANT
+                if (position.row == 4) {
+                    Position left = new Position(position.row, position.column - 1);
+                    if (board.validPosition(left) && enemyAhead(left) && board.piece(left) == match.vulnerableEnPassant) {
+                        boolMatrix[left.row + 1, left.column] = true;
+                    }
+                    Position right = new Position(position.row, position.column + 1);
+                    if (board.validPosition(right) && enemyAhead(right) && board.piece(right) == match.vulnerableEnPassant) {
+                        boolMatrix[right.row + 1, right.column] = true;
+                    }
                 }
             }
             
